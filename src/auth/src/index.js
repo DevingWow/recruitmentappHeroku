ba9d254b28273e1649925b7af13c1ba9c9dbdef0
Sahidev1 <theoddrogue@gmail.com>
2023-11-19 02:52:39 +0100

    git auth service code started

---
+++ src/auth/src/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const APP_ROOT_DIR = path.join(__dirname, '..');
+
+const DEFAULT_PORT = '8000';
+
+require('dotenv').config({
+    path: path.join(APP_ROOT_DIR, '.env')
+});
+
+
+const express = require('express');
+const app = express();
+
+
+const server = app.listen(
+    process.env.PORT||DEFAULT_PORT, () => {
+        console.log('Server is up');
+    }
+)
