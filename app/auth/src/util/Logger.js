677ee3cf7fd7ac7ba3753e20961a378aa01051ab
Sahidev1 <theoddrogue@gmail.com>
2023-12-14 20:23:41 +0100

    updated loggers accross all services

--- app/auth/src/util/Logger.js
+++ app/auth/src/util/Logger.js
@@ -2,29 +2,49 @@ const fs = require('fs');
 const path = require('path');
 
 class Logger {
-  constructor() {
-    this.logfilepath = path.join(__dirname, '..', 'log.txt');
-    console.log("LOG PATH: " + this.logfilepath);
-  }
+	constructor() {
+		this.logfilepath = path.join(__dirname, '..', 'log.txt');
+		console.log("LOG PATH: " + this.logfilepath);
+	}
 
-  logToConsole(message){
-    console.log(message);
-  }
+	logToConsole(message) {
+		console.log(message);
+	}
 
-  logToFile(message){
-    let date = new Date();
-    let timestamp = date.getTime();
-    message = date + " " + timestamp + ":\n" + message;
-    fs.appendFile(this.logfilepath, message, function (err) {
-      if (err) throw err;
-      console.log('Saved!');
-    });
-  }
+	async checkLogFileSize() {
+		const MAX_SIZE_MB = 1;
+		try {
+			let stats = await fs.promises.stat(this.logfilepath);
+			let fileSizeInBytes = stats.size;
+			let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+			if (fileSizeInMegabytes >= MAX_SIZE_MB) {
+				await fs.promises.truncate(this.logfilepath, 0);
+			}
+		} catch (error) {
+			throw error;
+		}
+	}
 
-  log(message){
-    this.logToConsole(message);
-    this.logToFile(message);
-  }
+	async logToFile(message) {
+		let date = new Date();
+		let timestamp = date.getTime();
+		message = date + " " + timestamp + ":\n" + message;
+		try {
+			await this.checkLogFileSize();
+			await fs.promises.appendFile(this.logfilepath, message);
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async log(message) {
+		try {
+			this.logToConsole(message);
+			await this.logToFile(message);
+		} catch (error) {
+			throw error;
+		}
+	}
 }
 
 const logger = new Logger();
